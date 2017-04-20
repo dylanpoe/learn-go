0ef2b2c769934f396454fa6d3a3d1ceb652a7f38
dylan <1121140449@qq.com>
2017-04-20 11:26:56 +0800

    random.go

---
+++ random.go
@@ -0,0 +1,11 @@
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandomString(length int) string {
+
+	//rand.Seed(time.Now().UTC().UnixNano())
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
